5331dd69b9f47b64bacd4707b3ae0ac3ec425e6b
berogio <giorgi.beridze01e@geolab.edu.ge>
2023-05-29 22:13:13 +0200

    Injected Service

--- src/app/widget/widget-content/widget-content.component.ts
+++ src/app/widget/widget-content/widget-content.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Bunus } from 'src/app/interfaces/bunus';
+import { BonusDataService } from 'src/app/services/bonus-data.service';
 
 @Component({
   selector: 'app-widget-content',
   templateUrl: './widget-content.component.html',
-  styleUrls: ['./widget-content.component.scss']
+  styleUrls: ['./widget-content.component.scss'],
 })
-export class WidgetContentComponent {
+export class WidgetContentComponent implements AfterViewInit {
+  authBonus: number = 0;
+  depositBonus: number = 0;
+  bigWin: number = 0;
+  constructor(private bonusDataService: BonusDataService) {}
 
+  ngAfterViewInit(): void {
+    this.getBonusData();
+  }
+
+  getBonusData() {
+    this.bonusDataService.getBonus().subscribe(
+      (data: Bunus[]) => {
+        this.authBonus = data[0].AuthBonus;
+        this.depositBonus = data[0].DepositBonus;
+        this.bigWin = data[0].BigWin;
+      },
+      (error) => {
+        console.error('Error fetching bonus data:', error);
+      }
+    );
+  }
 }
