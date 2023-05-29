8f0754dea2da71f1155425efc3c90a50dedef9c9
berogio <giorgi.beridze01e@geolab.edu.ge>
2023-05-30 01:14:02 +0200

    Added Directive and injected Elref, Rendere2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { WidgetComponent } from './widget/widget-container/widget-container.component';
 import { WidgetContentComponent } from './widget/widget-content/widget-content.component';
 import { SeparatorDirective } from './directives/separator.directive';
+import { DragDropDirective } from './directives/drag-drop.directive';
 
 @NgModule({
-  declarations: [AppComponent, WidgetComponent, WidgetContentComponent, SeparatorDirective],
+  declarations: [AppComponent, WidgetComponent, WidgetContentComponent, SeparatorDirective, DragDropDirective],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
